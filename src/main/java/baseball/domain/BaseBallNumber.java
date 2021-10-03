2412d5eb082dae7663c94facd6c911d973056f57
kbh0581 <kbh0581@gmail.com>
2021-10-04 00:58:45 +0900

    feat : 숫자야구숫자 생성, 유효성 검사 기능을 추가
    - 숫자야구의 숫자 생성과, 숫자에 대한 유효성을 담당하는 BaseBallNumber.class 추가

---
+++ src/main/java/baseball/domain/BaseBallNumber.java
@@ -0,0 +1,43 @@
+package baseball.domain;
+
+import java.util.LinkedHashSet;
+
+import baseball.util.ArraysUtil;
+
+
+public class BaseBallNumber {
+	private LinkedHashSet<Integer> ballNumbers;
+
+	BaseBallNumber(int[] ballNumbers) {
+		this.ballNumbers = createBallNumber(ballNumbers);
+	}
+
+	public LinkedHashSet<Integer> getBallNumbers() {
+		return this.ballNumbers;
+	}
+
+	private LinkedHashSet<Integer> createBallNumber(int[] ballNumberArr) {
+		LinkedHashSet<Integer> ballNumbers = ArraysUtil.convertIntegerArrayToSet(ballNumberArr);
+		validate(ballNumbers);
+		return ballNumbers;
+	}
+
+	public void changeBallNumber(int[] ballNumberArr) {
+		this.ballNumbers = createBallNumber(ballNumberArr);
+	}
+
+	private void validateNumber(int number) {
+		if (number > 9 || number < 1) {
+			throw new IllegalArgumentException("야구 숫자는 1 ~ 9 사이여야 합니다. 숫자 : " + number);
+		}
+	}
+
+	public void validate(LinkedHashSet<Integer> ballNumbers) {
+		if (ballNumbers.size() != 3) {
+			throw new IllegalArgumentException("야구 숫자는 세자리이며 중복된 수가 없어야 합니다.");
+		}
+		for (int ballNumber : ballNumbers) {
+			validateNumber(ballNumber);
+		}
+	}
+}
